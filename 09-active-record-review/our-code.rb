5227f0511926aada0892a246e4cf9904c4438951
clarkj99 <clark@stontech.net>
2019-10-18 23:33:12 -0400

    Nailed it

--- 09-active-record-review/our-code.rb
+++ 09-active-record-review/our-code.rb
@@ -1,44 +1,38 @@
-# Awesome! We've created some models and a database to work with. 
-# Let's add some content. 
+# Awesome! We've created some models and a database to work with.
+# Let's add some content.
 
-
+require_relative "./blog.rb"
 
 # Create a new post.
-
 post = Post.new
 
+# Give the post a title.
+post.name = "YEET!"
 
-# Give the post a title. 
-
-post.title = "YEET!"
-
-# Add some body content to the post. 
+# Add some body content to the post.
 
 post.content = "Today, we yeeted without our haw. Yesterday, the haw was yeeted."
-
 
 # Save the post to persist it in our local database (stored in RAM)
 
 post.save
 
-# Add a comment to our post. 
+# Add a comment to our post.
 
 post.comments.create(content: "I DON'T LIKE YEETIN' HAWS!!!")
 
-
-# Add another one! 
+# Add another one!
 
 post.comments.create(content: "I like yeetin' haws. :D")
 
+# Show EACH post and the comments for each in output.
 
-# Show EACH post and the comments for each in output. 
-
-post.find_each do |post|
-    p post.comments
+# binding.pry
+# Post.find_each do |post|
+#   p post.comments
+# end
+post.comments.find_each do |post|
+  p post.content
 end
 
-
-# What are some other CRUD operations that we can try? 
-
-
-
+# What are some other CRUD operations that we can try?
